e2c0c12653db6cb075a1775a9bdf551f2566cc85
Jian Li <jianli130@gmail.com>
2011-05-26 15:21:29 -0700

    Initial project

---
+++ fairview/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fairview::Application.config.secret_token = '50be0f8c6cfc6badd3ff2c29160d677dfdc824de5dbbacdca7128c86676ef4184fb748db6f281bb475f1f2e5d2b0359192ff18fde625bbd1f2ecbccae677be3b'
