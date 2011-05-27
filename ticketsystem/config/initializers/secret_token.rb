0ef9a841e19a9680d962895272065665bd80c5bf
Jian Li <jianli130@gmail.com>
2011-05-27 00:57:19 -0700

    add ticket system app

---
+++ ticketsystem/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ticketsystem::Application.config.secret_token = '3db6eb11cb405ac73ec40e71886256436884ad91d8e53dbe688ce04c86809248d086bfd694b40fb3e127b5b05d2bc63787bb9955347894fd7c6c0426a1f11095'
